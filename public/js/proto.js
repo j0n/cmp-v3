c7890629533ec92c625e79e7c01529dd856b2f77
Jon <jon@Jons-MacBook-Pro.local>
2013-04-21 17:17:56 +0200

    starting hammer

--- public/js/proto.js
+++ public/js/proto.js
@@ -1,14 +1,102 @@
-var amountLeft = 44;//get from API
-window.addEventListener('load', function(){
-  $('buy-amount').addEventListener('change', function(e){
-    if (this.value < 1) {
-      console.log('no less than 1');
-      e.preventDefault();
-      this.value = 1;
-    }
-    $('amount-tickets-left').innerHTML = amountLeft - this.value;
-  })
-});
-$ = function(id){
-  return document.getElementById(id);
+function a() {
+  document.getElementById('stage').addEventListener('mousedown', function(e){
+    console.log('statge licke', e);
+
+    var div = document.createElement('div');
+    div.className = 'block';
+    div.style.left = e.clientX + 'px';
+    div.style.top = e.clientY + 'px';
+    div.addEventListener('mousedown', function(e) {
+      var self = this;
+      document.addEventListener('mousemove',move(e, div));
+      e.stopPropagation();
+    });
+    div.addEventListener('mouseup', function(e) {
+      console.log('up');
+      document.removeEventListener('mousemove', move);
+    });
+    this.appendChild(div);
+  });
 }
+function move(e, div) {
+  div.style.left = e.clientX + 'px';
+  div.style.top = e.clientY + 'px';
+}
+$(function() {
+  var dragging = [];
+  var moving = false;
+  $(document).on('mousedown touchstart', function(e) {
+    if (!moving) {
+      if (typeof e.originalEvent.touches != 'undefined') {
+        for (var i = 0; i < e.originalEvent.touches.length; i++) {
+          var block = $('<div class="block"></div>');
+          block.css({
+            left: e.originalEvent.touches[i].pageX,
+            top: e.originalEvent.touches[i].pageY
+          });
+          $('#stage').append(block);
+          $(div).hammer({
+            drag: true,
+            transform: true
+          });
+        }
+      }
+      else {
+        var block = $('<div class="block"></div>');
+        block.css({
+          left: e.originalEvent.pageX,
+          top: e.originalEvent.pageY
+        });
+        $('#stage').append(block);
+      }
+    }
+    e.preventDefault();
+    return false;
+  });
+  $('#stage').on('mousedown touchstart', '.block', function(e) {
+    var self = this;
+    var currentWidth = $(this).width();
+    moving = true;
+    $(document).on('mousemove touchmove', function(e) {
+      if (typeof e.originalEvent.touches != 'undefined') {
+        e.originalEvent.touches[0]
+        $(self).css({
+          left: e.originalEvent.touches[0].pageX - (currentWidth/2),
+          top: e.originalEvent.touches[0].pageY - (currentWidth/2)
+        });
+
+      }
+      else {
+        $(self).css({
+          left: e.originalEvent.pageX,
+          top: e.originalEvent.pageY
+        });
+      }
+      e.preventDefault();
+      return false;
+    });
+    $(this).hammer().on('rotate', function() {
+      console.log('rotate');
+      $(self).css({
+        width: e.originalEvent.scale * currentWidth,
+        height: e.originalEvent.scale * currentWidth,
+        'webkitTransform': 'rotate('+e.originalEvent.rotation+'deg)'
+      });
+      e.preventDefault();
+      e.stopPropagation();
+    });
+    $(document).on('mouseup touchend', function(e) {
+      var remove = true;
+      if (typeof e.originalEvent.touches != 'undefined') {
+        if (e.originalEvent.touches.length != 0) {
+          remove = false;
+        }
+      }
+      if (remove) {
+        moving = false;
+        $(document).off('mousemove touchmove gesturechange');
+      }
+    });
+    e.stopPropagation();
+  });
+});
