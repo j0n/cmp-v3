818d48fd6acf64c31260f3df35cca7c6b286779d
Jon <jon@Jons-MacBook-Pro.local>
2013-04-22 11:58:54 +0200

    finished

--- public/js/proto.js
+++ public/js/proto.js
@@ -1,31 +1,10 @@
-function a() {
-  document.getElementById('stage').addEventListener('mousedown', function(e){
-    console.log('statge licke', e);
-
-    var div = document.createElement('div');
-    div.className = 'block';
-    div.style.left = e.clientX + 'px';
-    div.style.top = e.clientY + 'px';
-    div.addEventListener('mousedown', function(e) {
-      var self = this;
-      document.addEventListener('mousemove',move(e, div));
-      e.stopPropagation();
-    });
-    div.addEventListener('mouseup', function(e) {
-      console.log('up');
-      document.removeEventListener('mousemove', move);
-    });
-    this.appendChild(div);
-  });
-}
-function move(e, div) {
-  div.style.left = e.clientX + 'px';
-  div.style.top = e.clientY + 'px';
-}
 $(function() {
-  var dragging = [];
-  var moving = false;
-  $(document).on('mousedown touchstart', function(e) {
+  var moving = false; // variabel som är true när man flyttar, så det inte kommer till fler fyrkanter när man flyttar
+  var currentWidth = 50; // hur bred är fyrkanten som just nu flyttas
+  var finished = false; // har användaren börjat pincha? om så flytta inget mer
+
+  $('#stage').on('mousedown touchstart', function(e) { // skapa fyrkanter på klick och touch start
+
     if (!moving) {
       if (typeof e.originalEvent.touches != 'undefined') {
         for (var i = 0; i < e.originalEvent.touches.length; i++) {
@@ -35,10 +14,6 @@ $(function() {
             top: e.originalEvent.touches[i].pageY
           });
           $('#stage').append(block);
-          $(div).hammer({
-            drag: true,
-            transform: true
-          });
         }
       }
       else {
@@ -47,56 +22,56 @@ $(function() {
           left: e.originalEvent.pageX,
           top: e.originalEvent.pageY
         });
+        $(block).hammer();
         $('#stage').append(block);
       }
     }
     e.preventDefault();
     return false;
   });
-  $('#stage').on('mousedown touchstart', '.block', function(e) {
-    var self = this;
-    var currentWidth = $(this).width();
-    moving = true;
-    $(document).on('mousemove touchmove', function(e) {
-      if (typeof e.originalEvent.touches != 'undefined') {
-        e.originalEvent.touches[0]
-        $(self).css({
-          left: e.originalEvent.touches[0].pageX - (currentWidth/2),
-          top: e.originalEvent.touches[0].pageY - (currentWidth/2)
-        });
 
-      }
-      else {
-        $(self).css({
-          left: e.originalEvent.pageX,
-          top: e.originalEvent.pageY
+  $('#stage').hammer().on('dragend', '.block', function(e) {
+    moving = false;
+    $(document).off('transform');
+  });
+
+  $('#stage').hammer().on('mousedown touchstart dragstart drag', '.block', function(e) {
+    var self = this;
+    moving = true;
+
+    if (e.type == 'touchstart' || e.type == 'mousedown') {
+      currentWidth = $(this).width();
+      finished = false;
+
+      $(this).hammer().on('transform', function(e) {
+        finished = true;
+        $(this).css({
+          left: e.gesture.center.pageX - ((currentWidth/2) * e.gesture.scale),
+          top: e.gesture.center.pageY - ((currentWidth/2) * e.gesture.scale),
+          width: e.gesture.scale * currentWidth,
+          height: e.gesture.scale * currentWidth,
+          'webkitTransform': 'rotate('+e.gesture.rotation+'deg)'
         });
-      }
-      e.preventDefault();
-      return false;
-    });
-    $(this).hammer().on('rotate', function() {
-      console.log('rotate');
-      $(self).css({
-        width: e.originalEvent.scale * currentWidth,
-        height: e.originalEvent.scale * currentWidth,
-        'webkitTransform': 'rotate('+e.originalEvent.rotation+'deg)'
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
       });
-      e.preventDefault();
-      e.stopPropagation();
-    });
-    $(document).on('mouseup touchend', function(e) {
-      var remove = true;
-      if (typeof e.originalEvent.touches != 'undefined') {
-        if (e.originalEvent.touches.length != 0) {
-          remove = false;
-        }
+
+      $(this).hammer().on('transformend', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+      });
+    }
+    else {
+      if (!finished) {
+        $(this).css({
+          left: e.gesture.touches[0].pageX - (currentWidth/2),
+          top: e.gesture.touches[0].pageY - (currentWidth/2)
+        });
+        e.stopPropagation();
+        return false;
       }
-      if (remove) {
-        moving = false;
-        $(document).off('mousemove touchmove gesturechange');
-      }
-    });
-    e.stopPropagation();
+    }
   });
 });
